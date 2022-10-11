90ca72ab63825e91fa8fbcb13a605424938cdc8d
Linda-Imeri <lindaimmeri@gmail.com>
2022-10-11 17:39:02 +0200

    Initial Commit

---
+++ DailyScrums/DailyScrumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyScrumsApp.swift
+//  DailyScrums
+//
+//  Created by Linda Imeri on 11.10.22.
+//
+
+import SwiftUI
+
+@main
+struct DailyScrumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
