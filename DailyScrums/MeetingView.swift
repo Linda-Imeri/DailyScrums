dd0a6edea60b077eed80c7713515e2b2503647af
Linda-Imeri <lindaimmeri@gmail.com>
2022-10-14 20:27:10 +0200

    MeetingView UI created

--- DailyScrums/MeetingView.swift
+++ DailyScrums/MeetingView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        Text("Hello!")
-            .padding()
+        VStack{
+            ProgressView(value: 3, total: 20)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("500",systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                    Label("600",systemImage:  "hourglass.tophalf.fill")
+                }
+            }
+            ZStack{
+                Circle()
+                Text("CHELLA is speaking").foregroundColor(Color.white)
+            }
+            HStack{
+                Text("Speaker 2 of 10")
+                Spacer()
+                Image(systemName: "forward.fill")
+            }
+        }.background(Color.pink)
     }
 }
 
